eb76d0dfdeae96e3071b828426bd7e65ea5ec657
Corrado <corrado.lanera@gmail.com>
2021-10-11 23:13:09 +0200

    deck 2: importazione

---
+++ web/esercizi/02_ex-importazione.R
@@ -0,0 +1,21 @@
+
+# Pacchetti utilizzati --------------------------------------------
+
+
+
+
+
+# Importazione ----------------------------------------------------
+
+
+# Importare il file `messy_aoe.xlsx` che si trova dentro la cartella
+# `data-raw/` presente dentro la cartella principale del progetto
+# (di cui, per esempio, non conosciamo il nome).
+#
+# - codificare tutti i caratteri rappresentanti un dato mancante come
+#   NA
+# - rimuovere tutte le righe o colonne vuote
+# - convertire tutti i nomi di variabile utilizzando la convenzione
+#   "snake_case".
+
+
---
+++ web/esercizi/caso-studio-completo.R
@@ -0,0 +1,5 @@
+library(here)
+
+
+# Importa al meglio che puoi il file che trovi qui:
+here("data-raw/messy_aoe.xlsx")
